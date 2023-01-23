9510cf6d305351d28c8b4923ff59ee30707b7554
Anastasiya Prudnikova <Anasatasiya Prudnikova>
2023-01-22 20:12:41 -0600

    added more pages and more test scripts

--- src/test/java/scripts/TechGlobalMultipleWindowsTest.java
+++ src/test/java/scripts/TechGlobalMultipleWindowsTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.TechGlobalFrontendTestingHomePage;
 import pages.TechGlobalMultipleWindowsPage;
-import utilities.Waiter;
+import utilities.WindowHandler;
 
 public class TechGlobalMultipleWindowsTest extends TechGlobalBase{
 
@@ -20,26 +20,29 @@ public class TechGlobalMultipleWindowsTest extends TechGlobalBase{
 
     @Test(priority = 1, description = "Validate the Apple link")
     public void validateTheAppleLink(){
-        Waiter.pause(2);
-        // 1 window/tab
-        String mainWindow = driver.getWindowHandle();
         techGlobalMultipleWindowsPage.links.get(0).click();
+        WindowHandler.switchToChildWindow();
 
-        // 2 windows/tabs
-        for (String windowId : driver.getWindowHandles()) {
-            if(!windowId.equals(mainWindow)){
-                driver.switchTo().window(windowId);
-                break;
-            }
-        }
-        Assert.assertEquals(driver.getTitle(),"Apple");
+        Assert.assertEquals(driver.getTitle(), "Apple");
         driver.close();
 
-        // go back to TechGlobal page and validate that title is TechGlobal
-        driver.switchTo().window(mainWindow);
+        WindowHandler.switchBackToParentWindow();
+
         Assert.assertTrue(driver.getCurrentUrl().contains("techglobal"));
+    }
 
+    @Test(priority = 2, description = "Validate link urls")
+    public void validateLinkURLs(){
+        String[] urls = {"https://www.apple.com/", "https://www.microsoft.com/en-us/", "https://www.tesla.com/"};
 
+        for (int i = 0; i < techGlobalMultipleWindowsPage.links.size(); i++) {
+            techGlobalMultipleWindowsPage.links.get(i).click();
 
+            WindowHandler.switchToChildWindow();
+
+            Assert.assertEquals(driver.getCurrentUrl(), urls[i]);
+            driver.close();
+            WindowHandler.switchBackToParentWindow();
+        }
     }
 }
