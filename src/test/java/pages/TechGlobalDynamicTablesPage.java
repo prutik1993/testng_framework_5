9843fced53308bc7b2cb170b817d7a98c3c0c874
Anastasiya Prudnikova <Anasatasiya Prudnikova>
2023-01-29 13:42:51 -0600

    learned dynamic table validation

--- src/test/java/pages/TechGlobalDynamicTablesPage.java
+++ src/test/java/pages/TechGlobalDynamicTablesPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class TechGlobalDynamicTablesPage extends TechGlobalBasePage{
     public TechGlobalDynamicTablesPage(){
         super();
@@ -12,5 +14,14 @@ public class TechGlobalDynamicTablesPage extends TechGlobalBasePage{
     public WebElement addProductButton;
 
     @FindBy(id = "modal_title")
-    public WebElement modalCard;
+    public WebElement modalCardTitle;
+
+    @FindBy(css = ".input")
+    public List<WebElement> productDetails;
+    @FindBy(id = "submit")
+    public WebElement submitButton;
+    @FindBy(id = "total_amount")
+    public WebElement totalAmount;
+    @FindBy(css = "tbody td:nth-child(1)")
+    public List<WebElement> tableRow;
 }
