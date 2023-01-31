3aab85e8bafaf8428241875e552ae9dacc5a5c4c
Anastasiya Prudnikova <Anasatasiya Prudnikova>
2023-01-31 15:00:02 -0600

    created more test scripts for sortable tables

--- src/test/java/scripts/TechGlobalSortableTablesTest.java
+++ src/test/java/scripts/TechGlobalSortableTablesTest.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 import pages.TechGlobalFrontendTestingHomePage;
 import pages.TechGlobalSortableTablesPage;
 import utilities.TableData;
+import utilities.TextHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TechGlobalSortableTablesTest extends TechGlobalBase{
@@ -28,18 +28,78 @@ public class TechGlobalSortableTablesTest extends TechGlobalBase{
      * Click on the Quantity sort button to toggle Ascending Order
      * Verify that the Quantity column of the table is sorted in ascending order
      */
-    @Test(priority = 1, description = "Sort table by asc and valiadte it is sorted by quantity ")
+    @Test(priority = 1, description = "Sort table by asc and validate it is sorted by quantity ")
     public void validateSortAscByQuantity(){
         techGlobalSortableTablesPage.ascByQuantity.click();
 
         List<WebElement> quantityColumnElements = TableData.getTableColumn(driver,1);
-        List<Integer> quantityColumn = new ArrayList<>();
 
-        for (WebElement element : quantityColumnElements) {
-            quantityColumn.add((Integer.parseInt(element.getText())));
-        }
+        List<Integer> quantityColumn = TextHandler.returnListOfInteger(quantityColumnElements);
+
         for (int i = 1; i < quantityColumn.size(); i++) {
             Assert.assertTrue(quantityColumn.get(i) >= quantityColumn.get(i - 1));
         }
     }
+    /**
+     * TEST2
+     * Go to https://techglobal-training.netlify.app/
+     * Click on "Practices" dropdown in the header
+     * Select the "Frontend Testing" option
+     * Click on the "Sortable Tables" card
+     * Click on the Quantity sort button to toggle descending order
+     * Verify that the Quantity column of the table is sorted in descending order
+     */
+    @Test(priority = 2, description = "Validate that table quantity  data is sorted by descending order")
+    public void validateSortedDescendingOrder(){
+        techGlobalSortableTablesPage.descByQuantity.click();
+
+        List<WebElement> quantityColumnElements = TableData.getTableColumn(driver,1);
+        List<Integer> quantityColumn = TextHandler.returnListOfInteger(quantityColumnElements);
+
+        for (int i = 1; i < quantityColumn.size(); i++) {
+           Assert.assertTrue(quantityColumn.get(i) <= quantityColumn.get(i - 1));
+        }
+    }
+    /**
+     * TEST3
+     * Go to https://techglobal-training.netlify.app/
+     * Click on "Practices" dropdown in the header
+     * Select the "Frontend Testing" option
+     * Click on the "Sortable Tables" card
+     * Click on the Price sort button to toggle Ascending Order
+     * Verify that the Price column of the table is sorted in ascending order
+     */
+    @Test(priority = 3,description = "Validate that table data is sorted in ascending order in price column")
+    public void validateAscendingOrderInPriceColumn(){
+        techGlobalSortableTablesPage.ascByPrice.click();
+
+        List<WebElement> priceColumnElements = TableData.getTableColumn(driver,3);
+        List<Integer> priceColumn = TextHandler.returnListOfInteger(priceColumnElements);
+
+        for (int i = 0; i < priceColumn.size()-1; i++) {
+            Assert.assertTrue(priceColumn.get(i) <= priceColumn.get(i + 1));
+        }
+    }
+
+    /**
+     * TEST4
+     * Go to https://techglobal-training.netlify.app/
+     * Click on "Practices" dropdown in the header
+     * Select the "Frontend Testing" option
+     * Click on the "Sortable Tables" card
+     * Click on the Price sort button to toggle descending order
+     * Verify that the Price column of the table is sorted in descending order
+     */
+    @Test(priority = 3,description = "Validate that table data is sorted in ascending order in price column")
+    public void validateDescendingOrderInPriceColumn(){
+        techGlobalSortableTablesPage.descByPrice.click();
+
+        List<WebElement> priceColumnElements = TableData.getTableColumn(driver,3);
+        List<Integer> priceColumn = TextHandler.returnListOfInteger(priceColumnElements);
+
+        for (int i = 0; i < priceColumn.size()-1; i++) {
+            Assert.assertTrue(priceColumn.get(i) >= priceColumn.get(i + 1));
+        }
+    }
+
 }
--- src/test/java/utilities/TextHandler.java
+++ src/test/java/utilities/TextHandler.java
@@ -1,7 +1,19 @@
 package utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextHandler {
     public static int getInt(String str){
         return Integer.parseInt(str.replaceAll("[^0-9]",""));
+    }
+    public static List<Integer> returnListOfInteger(List<WebElement> list){
+        List<Integer> answer = new ArrayList<>();
+        for (WebElement element : list) {
+         answer.add(Integer.parseInt(element.getText()));
+        }
+        return answer;
     }
 }
